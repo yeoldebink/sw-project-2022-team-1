df9dbd8bd16c7f6462f2073a47f7d891167bbcf0
Or Bairey-Sehayek <orbairey@gmail.com>
2021-12-07 21:28:18 +0200

    test for ci

--- src/test/java/il/ac/haifa/hmo_system/AppTest.java
+++ src/test/java/il/ac/haifa/hmo_system/AppTest.java
@@ -1,8 +1,8 @@
 package il.ac.haifa.hmo_system;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /** Unit test for simple App. */
 public class AppTest {
