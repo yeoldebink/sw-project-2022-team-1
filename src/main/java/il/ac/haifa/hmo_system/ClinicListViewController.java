5bb07ae246687337feb176af6c3969c8f5755a38
Or Bairey-Sehayek <orbairey@gmail.com>
2021-12-09 23:31:08 +0200

    ClinicListView FXML + Controller

---
+++ src/main/java/il/ac/haifa/hmo_system/ClinicListViewController.java
@@ -0,0 +1,16 @@
+package il.ac.haifa.hmo_system;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class ClinicListViewController {
+
+  @FXML private TableView<?> clinicTable;
+
+  @FXML
+  void showAddClinicDialog(ActionEvent event) {}
+
+  @FXML
+  void showEditClinicDialog(ActionEvent event) {}
+}
