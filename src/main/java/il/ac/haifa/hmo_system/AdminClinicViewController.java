37e3ec190e808ec98fb39640db924ca9daf753e6
Or Bairey-Sehayek <orbairey@gmail.com>
2021-12-11 00:02:29 +0200

    Added AdminClinicView

---
+++ src/main/java/il/ac/haifa/hmo_system/AdminClinicViewController.java
@@ -0,0 +1,17 @@
+package il.ac.haifa.hmo_system;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class AdminClinicViewController {
+
+  public TextField sunHoursTextField;
+  public TextField monHoursTextField;
+  public TextField tueHoursTextField;
+  public TextField wedHoursTextField;
+  public TextField thuHoursTextField;
+  public TextField friHoursTextField;
+  public TextField satHoursTextField;
+
+  public void saveClinicChanges(ActionEvent actionEvent) {}
+}
