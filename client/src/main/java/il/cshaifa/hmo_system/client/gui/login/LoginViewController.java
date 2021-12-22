b6accc1bee3c16aa9746efae524dff993772f5ef
Or Bairey-Sehayek <obaireys@redhat.com>
2021-12-22 17:57:22 +0200

    Created FXML and skeleton

---
+++ client/src/main/java/il/cshaifa/hmo_system/client/gui/login/LoginViewController.java
@@ -0,0 +1,20 @@
+package il.cshaifa.hmo_system.client.gui.login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginViewController {
+
+  @FXML
+  private TextField idTextField;
+
+  @FXML
+  private PasswordField passwordField;
+
+  @FXML
+  void requestLogin(ActionEvent event) {
+
+  }
+}
