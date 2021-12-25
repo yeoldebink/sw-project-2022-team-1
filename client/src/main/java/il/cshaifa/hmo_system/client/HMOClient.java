a16748bea9ffd838b6ba2efed8cd0a8ebf809b85
nirvataro <nvataro@gmail.com>
2021-12-25 21:58:42 +0200

    cast

--- client/src/main/java/il/cshaifa/hmo_system/client/HMOClient.java
+++ client/src/main/java/il/cshaifa/hmo_system/client/HMOClient.java
@@ -38,7 +38,9 @@ public class HMOClient extends AbstractClient {
     return client;
   }
 
-  /** @param msg the message sent. This message can be of several types, handled by controller. */
+  /**
+   * @param msg the message sent. This message can be of several types, handled by controller.
+   */
   @Override
   protected void handleMessageFromServer(Object message) {
     this.msg = true;
@@ -79,7 +81,7 @@ public class HMOClient extends AbstractClient {
   }
 
   /**
-   * @param user The id of the login request
+   * @param user     The id of the login request
    * @param password The password the user has entered
    * @throws IOException SQL exception
    */
