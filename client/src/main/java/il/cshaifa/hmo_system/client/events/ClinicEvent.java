d230abcc8d2abf7ef77ff159f985974d68748821
Liran <liranbd1@gmail.com>
2021-12-28 20:21:48 +0200

    No messages are sent between client objects

--- client/src/main/java/il/cshaifa/hmo_system/client/events/ClinicEvent.java
+++ client/src/main/java/il/cshaifa/hmo_system/client/events/ClinicEvent.java
@@ -4,6 +4,7 @@ import il.cshaifa.hmo_system.entities.Clinic;
 import java.util.ArrayList;
 
 public class ClinicEvent {
+<<<<<<< HEAD
   public ArrayList<Clinic> receivedClinics;
   public Clinic clinic;
   public Phase phase;
@@ -29,4 +30,31 @@ public class ClinicEvent {
     REQUEST,
     LIST
   }
+=======
+    public ArrayList<Clinic> receivedClinics;
+    public Clinic clinic;
+    public Phase phase;
+
+    public ClinicEvent(){
+        this.phase = Phase.REQUEST;
+    }
+
+    public ClinicEvent(ArrayList<Clinic> clinics){
+        this.receivedClinics = clinics;
+        this.clinic = null;
+        this.phase = Phase.LIST;
+    }
+
+    public ClinicEvent (Clinic clinic, Phase phase){
+        this.clinic = clinic;
+        this.receivedClinics = null;
+        this.phase = phase;
+    }
+
+    public enum Phase{
+        EDIT,
+        REQUEST,
+        LIST
+    }
+>>>>>>> ecc298d (No messages are sent between client objects)
 }
