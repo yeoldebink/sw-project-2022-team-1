6deaafc584ad3d6918f20bff16e61c8aa5884f76
nirvataro <nvataro@gmail.com>
2021-12-30 00:56:21 +0200

    google....

--- server/src/main/java/il/cshaifa/hmo_system/server/HMOServer.java
+++ server/src/main/java/il/cshaifa/hmo_system/server/HMOServer.java
@@ -138,7 +138,9 @@ public class HMOServer extends AbstractServer {
     client.sendToClient(clinics_msg);
   }
 
-  /** @param entity_list Entities to be updated to DB */
+  /**
+   * @param entity_list Entities to be updated to DB
+   */
   protected void updateEntities(List<?> entity_list) {
     for (var entity : entity_list) {
       session.update(entity);
@@ -158,7 +160,7 @@ public class HMOServer extends AbstractServer {
    * made changes to this clinics and apply changes to DB
    *
    * @param message ClinicMessage
-   * @param client The client that made the request
+   * @param client  The client that made the request
    * @throws IOException SQL exception
    */
   protected void handleClinicMessage(ClinicMessage message, ConnectionToClient client)
@@ -182,8 +184,8 @@ public class HMOServer extends AbstractServer {
    * If login successful will send to client LoginMessage with user and his details
    *
    * @param message LoginMassage should be with user_id and password
-   * @param client The client that request the login
-   * @throws IOException SQL exception
+   * @param client  The client that request the login
+   * @throws IOException              SQL exception
    * @throws NoSuchAlgorithmException Encoding password exception
    */
   protected void handleLogin(LoginMessage message, ConnectionToClient client)
@@ -213,7 +215,7 @@ public class HMOServer extends AbstractServer {
   /**
    * See documentation for entities.Request for defined behavior.
    *
-   * @param msg the message sent.
+   * @param msg    the message sent.
    * @param client the connection connected to the client that sent the message.
    */
   @Override
