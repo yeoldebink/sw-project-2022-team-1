d8370f6cf001791ec341fe3a37cee9b453356db9
Tomer <tomerb93@gmail.com>
2022-01-08 12:49:16 +0200

    Removed close button func ~ added label+column text by report type ~ fxml changes

--- client/src/main/java/il/cshaifa/hmo_system/client/gui/manager_dashboard/clinic_administration/report_view/ReportViewController.java
+++ client/src/main/java/il/cshaifa/hmo_system/client/gui/manager_dashboard/clinic_administration/report_view/ReportViewController.java
@@ -1,51 +1,65 @@
 package il.cshaifa.hmo_system.client.gui.manager_dashboard.clinic_administration.report_view;
 
 import il.cshaifa.hmo_system.client.base_controllers.ViewController;
+import il.cshaifa.hmo_system.messages.ReportMessage.ReportType;
+
 import java.util.ArrayList;
 import java.util.Map;
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+
 public class ReportViewController extends ViewController {
-  @FXML private TableView<ReportDataRow> report_table;
-  @FXML private Label report_type_label;
-  @FXML private TableColumn<ReportDataRow, String> key;
-  @FXML private TableColumn<ReportDataRow, Double> value;
-  @FXML private Button close_button;
+    @FXML private TableView<ReportDataRow> report_table;
+    @FXML private Label report_type_label;
+    @FXML private TableColumn<ReportDataRow, String> key;
+    @FXML private TableColumn<ReportDataRow, Double> value;
 
-  // TODO: How to add type of report to parameters if ReportType is defined in server only
-  public ReportViewController() {}
+    private ReportType report_type;
 
-  @FXML
-  public void initialize() {
-    setCellValueFactory();
-    // TODO Tomer: Set view label and table column text according to report type (data can be
-    // transparent to view)
-  }
-
-  @FXML
-  void requestCloseWindow(ActionEvent event) {
-    closeWindow(event);
-  }
-
-  void populateReportTable(Map<String, Double> report_data) {
-
-    ArrayList<ReportDataRow> report_data_rows = new ArrayList<ReportDataRow>();
-
-    for (Map.Entry<String, Double> entry : report_data.entrySet()) {
-      report_data_rows.add(new ReportDataRow(entry.getKey(), entry.getValue()));
+    public ReportViewController(ReportType report_type) {
+        this.report_type = report_type;
     }
 
-    report_table.getItems().setAll(report_data_rows);
-  }
+    @FXML
+    public void initialize() {
+        setCellValueFactory();
 
-  private void setCellValueFactory() {
-    key.setCellValueFactory((new PropertyValueFactory<>("Key")));
-    value.setCellValueFactory((new PropertyValueFactory<>("Value")));
-  }
+        processReportType();
+    }
+
+    private void processReportType() {
+        switch (this.report_type) {
+            case MISSED_APPOINTMENTS -> this.setViewText("Missed Appointments", "Type", "Amount missed");
+            case AVERAGE_WAIT_TIMES -> this.setViewText("Average Doctor Wait Times", "Doctor name", "Wait time");
+            case APPOINTMENT_ATTENDANCE -> this.setViewText("Appointment Attendance", "Type", "Num. attended");
+            default -> throw new IllegalStateException("Unexpected report type: " + this.report_type);
+        }
+    }
+
+    private void setViewText(String report_type_label_text, String key_column_text, String value_column_text) {
+        this.report_type_label.setText(report_type_label_text);
+        this.key.setText(key_column_text);
+        this.value.setText(value_column_text);
+    }
+
+    void populateReportTable(Map<String, Double> report_data) {
+
+        ArrayList<ReportDataRow> report_data_rows = new ArrayList<ReportDataRow>();
+
+        for (Map.Entry<String, Double> entry : report_data.entrySet()) {
+            report_data_rows.add(new ReportDataRow(entry.getKey(), entry.getValue()));
+        }
+
+        report_table.getItems().setAll(report_data_rows);
+    }
+
+    private void setCellValueFactory() {
+        key.setCellValueFactory((new PropertyValueFactory<>("Key")));
+        value.setCellValueFactory((new PropertyValueFactory<>("Value")));
+    }
 }
