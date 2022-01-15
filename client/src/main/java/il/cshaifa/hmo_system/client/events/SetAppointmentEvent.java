b235dcd1c4260395fe2b934bcee9cab4557d6d33
nirvataro <nvataro@gmail.com>
2022-01-15 12:20:44 +0200

    added work hours string to LocalTime

--- client/src/main/java/il/cshaifa/hmo_system/client/events/SetAppointmentEvent.java
+++ client/src/main/java/il/cshaifa/hmo_system/client/events/SetAppointmentEvent.java
@@ -4,12 +4,14 @@ import il.cshaifa.hmo_system.entities.Appointment;
 import il.cshaifa.hmo_system.entities.AppointmentType;
 import il.cshaifa.hmo_system.entities.Patient;
 import il.cshaifa.hmo_system.entities.Role;
+import java.util.List;
 
 public class SetAppointmentEvent extends Event {
   public enum Action {
     LOCK,
     TAKE,
     RELEASE,
+    ROLES_RESPONSE,
     AUTHORIZE,
     REJECT
   }
@@ -19,6 +21,7 @@ public class SetAppointmentEvent extends Event {
   public Appointment appointment;
   public AppointmentType appointmentType;
   public Role role;
+  public List<Role> specialistRoles;
 
   public SetAppointmentEvent(
       Object sender, Action action, Patient patient, Appointment appointment) {
@@ -26,5 +29,11 @@ public class SetAppointmentEvent extends Event {
     this.action = action;
     this.patient = patient;
     this.appointment = appointment;
+  }
+
+  public SetAppointmentEvent(Object sender, List<Role> role_list){
+    super(sender);
+    action = Action.ROLES_RESPONSE;
+    specialistRoles = role_list;
   }
 }
