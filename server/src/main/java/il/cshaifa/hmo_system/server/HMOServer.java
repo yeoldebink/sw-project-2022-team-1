fb4762953861fef1b19cf02362604878d0e40f7a
OrelMarom <72601494+OrelMarom@users.noreply.github.com>
2022-01-15 22:06:24 +0200

    handle set specialist appointment message - done

--- server/src/main/java/il/cshaifa/hmo_system/server/HMOServer.java
+++ server/src/main/java/il/cshaifa/hmo_system/server/HMOServer.java
@@ -36,6 +36,7 @@ import il.cshaifa.hmo_system.server.server_handlers.handleClinicMessage;
 import il.cshaifa.hmo_system.server.server_handlers.handleLoginMessage;
 import il.cshaifa.hmo_system.server.server_handlers.handleReportMessage;
 import il.cshaifa.hmo_system.server.server_handlers.handleSetAppointmentMessage;
+import il.cshaifa.hmo_system.server.server_handlers.handleSetSpecialistAppointmentMessage;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.time.DayOfWeek;
@@ -158,75 +159,6 @@ public class HMOServer extends AbstractServer {
     client.sendToClient(msg);
   }
 
-
-
-
-  private List<Role> getSpecialistRoleList() {
-    CriteriaBuilder cb = session.getCriteriaBuilder();
-    CriteriaQuery<Role> cr = cb.createQuery(Role.class);
-    Root<Role> root = cr.from(Role.class);
-    cr.select(root).where(cb.isTrue(root.get("is_specialist")));
-    return session.createQuery(cr).getResultList();
-  }
-
-  private List<Appointment> getSpecialistAppointments(Role role, Patient patient) {
-    CriteriaBuilder cb = session.getCriteriaBuilder();
-    CriteriaQuery<Appointment> cr = cb.createQuery(Appointment.class);
-    Root<Appointment> root = cr.from(Appointment.class);
-
-    cr.select(root).where(
-        cb.equal(root.get("patient"), patient),
-        cb.equal(root.get("specialist_role_id"), role),
-        cb.lessThan(root.get("appt_date"), LocalDateTime.now()),
-        cb.isNotNull(root.get("called_time"))
-    );
-    cr.orderBy(cb.desc(root.get("appt_date")));
-    List<Appointment> patient_past_appts = session.createQuery(cr).getResultList();
-
-    List<Appointment> available_appts = new ArrayList<Appointment>();
-
-    Set<User> doctors = new HashSet<>();
-    for (Appointment appt : patient_past_appts) {
-      if (!doctors.contains(appt.getStaff_member())) {
-        cr.select(root).where(
-            cb.equal(root.get("staff_member"), appt.getStaff_member()),
-            cb.equal(root.get("specialist_role_id"), role),
-            cb.between(root.get("appt_date"), LocalDateTime.now(),
-                LocalDateTime.now().plusMonths(3)),
-            cb.isFalse(root.get("taken")),
-            cb.or(cb.isNull(root.get("lock_time")),
-                cb.lessThan(root.get("lock_time"), LocalDateTime.now()))
-        );
-        doctors.add(appt.getStaff_member());
-        available_appts.addAll(session.createQuery(cr).getResultList());
-      }
-    }
-
-    cr.select(root).where(
-        cb.equal(root.get("specialist_role_id"), role),
-        root.get("staff_member").in(doctors).not(),
-        cb.between(root.get("appt_date"), LocalDateTime.now(), LocalDateTime.now().plusMonths(3)),
-        cb.isFalse(root.get("taken")),
-        cb.or(cb.isNull(root.get("lock_time")),
-            cb.lessThan(root.get("lock_time"), LocalDateTime.now()))
-    );
-
-    available_appts.addAll(session.createQuery(cr).getResultList());
-    return available_appts;
-  }
-
-  private void handleSetSpecialistAppointmentMessage(SetSpecialistAppointmentMessage msg,
-      ConnectionToClient client)
-      throws IOException {
-    if (msg.action == SetSpecialistAppointmentMessage.Action.GET_ROLES) {
-      msg.role_list = getSpecialistRoleList();
-    } else if (msg.action == SetSpecialistAppointmentMessage.Action.GET_APPOINTMENTS) {
-      msg.appointments = getSpecialistAppointments(msg.chosen_role, msg.patient);
-    }
-    msg.message_type = MessageType.RESPONSE;
-    client.sendToClient(msg);
-  }
-
   /**
    * See documentation for entities.Request for defined behavior.
    *
@@ -253,9 +185,9 @@ public class HMOServer extends AbstractServer {
         handler = new handleReportMessage((ReportMessage) msg, session);
       } else if (msg_class == SetAppointmentMessage.class) {
         handler = new handleSetAppointmentMessage((SetAppointmentMessage) msg, session);
-      } //else if (msg_class == SetSpecialistAppointmentMessage.class) {
-//        handler = new handleSetSpecialistAppointmentMessage((SetSpecialistAppointmentMessage) msg, session);
-//      } else if (msg_class == StaffAssignmentMessage.class) {
+      } else if (msg_class == SetSpecialistAppointmentMessage.class) {
+        handler = new handleSetSpecialistAppointmentMessage((SetSpecialistAppointmentMessage) msg, session);
+      } //else if (msg_class == StaffAssignmentMessage.class) {
 //        handler = new handleStaffAssignmentMessage((StaffAssignmentMessage) msg, session);
 //      } else if (msg_class == ClinicStaffMessage.class) {
 //        handler = new handleStaffMessage((ClinicStaffMessage) msg, session);
