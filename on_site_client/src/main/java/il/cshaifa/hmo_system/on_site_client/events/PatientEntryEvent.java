07a5f3796d43251da41f94dbdaf56cb108b74e9a
Or Bairey-Sehayek <orbairey@gmail.com>
2022-01-28 01:58:06 +0200

    Completed on-site events

--- on_site_client/src/main/java/il/cshaifa/hmo_system/on_site_client/events/PatientEntryEvent.java
+++ on_site_client/src/main/java/il/cshaifa/hmo_system/on_site_client/events/PatientEntryEvent.java
@@ -3,12 +3,12 @@ package il.cshaifa.hmo_system.on_site_client.events;
 import il.cshaifa.hmo_system.client_base.events.Event;
 import il.cshaifa.hmo_system.entities.Appointment;
 import il.cshaifa.hmo_system.entities.Patient;
+import il.cshaifa.hmo_system.structs.QueuedAppointment;
 
 public class PatientEntryEvent extends Event {
 
     public int id;
-    public Appointment appointment;
-    public String number_in_line;
+    public QueuedAppointment q_appt;
     public Patient patient; // if null, login was invalid
 
     private PatientEntryEvent(int id, Object sender) {
@@ -16,10 +16,9 @@ public class PatientEntryEvent extends Event {
         this.id = id;
     }
 
-    private PatientEntryEvent(Appointment appointment, String number_in_line, Patient patient, Object sender) {
+    private PatientEntryEvent(QueuedAppointment q_appt, Patient patient, Object sender) {
         super(sender);
-        this.appointment = appointment;
-        this.number_in_line = number_in_line;
+        this.q_appt = q_appt;
         this.patient = patient;
     }
 
@@ -27,7 +26,7 @@ public class PatientEntryEvent extends Event {
         return new PatientEntryEvent(id, sender);
     }
 
-    public static PatientEntryEvent entryResponseEvent(Appointment appointment, String number_in_line, Patient patient, Object sender) {
-        return new PatientEntryEvent(appointment, number_in_line, patient, sender);
+    public static PatientEntryEvent entryResponseEvent(QueuedAppointment q_appt, Patient patient, Object sender) {
+        return new PatientEntryEvent(q_appt, patient, sender);
     }
 }
