7f454f5c9a9f32209eb082eccb35c38ae04ad263
Tomer <tomerb93@gmail.com>
2022-01-28 15:51:27 +0200

    init staffappointmentlistview component

---
+++ on_site_client/src/main/java/il/cshaifa/hmo_system/on_site_client/gui/staff_appointment_list/StaffAppointmentListViewController.java
@@ -0,0 +1,7 @@
+package il.cshaifa.hmo_system.on_site_client.gui.staff_appointment_list;
+
+import il.cshaifa.hmo_system.client_base.base_controllers.ViewController;
+
+public class StaffAppointmentListViewController extends ViewController {
+
+}
