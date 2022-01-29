a37f65004d7a238d383beaea65c53862238304b3
Or Bairey-Sehayek <orbairey@gmail.com>
2022-01-29 17:22:30 +0200

    Prepared classes and views for current appointment view

---
+++ on_site_client/src/main/java/il/cshaifa/hmo_system/on_site_client/gui/staff/StaffAppointmentController.java
@@ -0,0 +1,14 @@
+package il.cshaifa.hmo_system.on_site_client.gui.staff;
+
+import il.cshaifa.hmo_system.client_base.base_controllers.Controller;
+import il.cshaifa.hmo_system.client_base.base_controllers.ViewController;
+import javafx.stage.Stage;
+
+public class StaffAppointmentController extends Controller {
+
+  public StaffAppointmentController(
+      ViewController view_controller,
+      Stage stage) {
+    super(view_controller, stage);
+  }
+}
---
+++ on_site_client/src/main/java/il/cshaifa/hmo_system/on_site_client/gui/staff/StaffAppointmentViewController.java
@@ -0,0 +1,7 @@
+package il.cshaifa.hmo_system.on_site_client.gui.staff;
+
+import il.cshaifa.hmo_system.client_base.base_controllers.ViewController;
+
+public class StaffAppointmentViewController extends ViewController {
+
+}
